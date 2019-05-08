23847f37495f21354bdd6eea7cf9751f9027e5b3
Justin McMurdie <mcmurdiej@gmail.com>
2019-05-08 10:58:19 -0600

    initial work on polling the server after we're told that we are offline

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { online$ } from './online-listener.js'
 
 export const onlineListener = online$
+export { setPollingUrl } from './online-listener.js'
--- src/online-listener.js
+++ src/online-listener.js
@@ -1,6 +1,8 @@
-import { ReplaySubject, interval } from 'rxjs'
+import { ReplaySubject, interval, from } from 'rxjs'
+import { switchMap, tap, filter } from 'rxjs/operators'
 
 const onlineSubject$ = new ReplaySubject(1)
+let urlToHit = 'https://google.com'
 
 onlineSubject$.next(true)
 
@@ -10,7 +12,42 @@ function updateOnlineStatus (evt) {
   }
 }
 
-window.addEventListener('online', updateOnlineStatus)
 window.addEventListener('offline', updateOnlineStatus)
 
 export const online$ = onlineSubject$.asObservable()
+export function setPollingUrl (url) {
+  urlToHit = url
+}
+
+function checkIfOnline () {
+  return fetch(urlToHit)
+}
+
+let pollingSubscription
+
+const isOfflineSub = online$.pipe(
+  tap((v) => console.log('v', v)),
+  filter(onlineBool => onlineBool === false),
+  tap(() => createPoll())
+).subscribe()
+
+function createPoll() {
+  clearPoll()
+  pollingSubscription = interval(5000).pipe(
+    switchMap(() => from(checkIfOnline()))
+  ).subscribe(
+    (results) => {
+      if (results.status === 200) {
+        onlineSubject$.next(true)
+        clearPoll()
+      }
+    }
+  )
+}
+
+function clearPoll() {
+  console.log('clearPoll')
+  if (pollingSubscription && pollingSubscription.unsubscribe) {
+    pollingSubscription.unsubscribe()
+  }
+}
